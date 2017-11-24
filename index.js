426666bffebdb8264ced6a98db47fa020152f5ba
Jordi Hereu <jhereumayo@gmail.com>
2017-11-24 12:00:37 +0100

    Initial commit of node-globals@1.0.0

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * @namespace GLOBAL-STORAGE MODULE
+ * @author Jordi Hereu <https://github.com/jhereu>
+ * @description Node module that let's you store data at a global scope.
+ * @version 1.0
+ */
+
+var _ = require('underscore');
+
+var GLOBALS = {};
+
+exports.set = function (key, val) {
+    if (_.isObject(key) && !val) {
+        GLOBALS = _.extend(GLOBALS, key);
+        return;
+    }
+    GLOBALS[key] = val;
+    return;
+};
+
+exports.get = function (key) {
+    return GLOBALS[key];
+};
+
+exports.list = function () {
+    return GLOBALS;
+};
+
+exports.flush = function () {
+    GLOBALS = {};
+    return;
+};
+
+exports.isSet = function (key) {
+    return _.has(GLOBALS, key);
+};
+
+exports.unset = function (key) {
+    GLOBALS = _.omit(GLOBALS, key);
+    return;
+};
